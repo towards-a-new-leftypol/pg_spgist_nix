f5e56517116aa8b394253db1c252c6f0105c4ec6
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2021-09-24 10:34:59 +0000

    Initial - commit default.nix

---
+++ default.nix
@@ -0,0 +1,33 @@
+# with import <nixpkgs> {};
+{ stdenv, fetchFromGitHub, postgresql }:
+
+let
+  name = "pg-spgist_hamming";
+  repo = fetchFromGitHub {
+    owner  = "fake-name";
+    repo   = name;
+    rev    = "9fa70b08e0f0108de6a6673ce095c86a987d261d";
+    sha256 = "0rh1l2saw2cnrn4mbawx42wx4a1343fjg3qxlcrgcs6aig7cpgfi";
+  };
+
+in
+
+stdenv.mkDerivation rec {
+    pname = name;
+
+    version = "1.0";
+
+    buildInputs = [
+      postgresql
+    ];
+
+    src = "${repo}/bktree";
+
+    installPhase = ''
+      mkdir -p $out/{lib,share/postgresql/extension}
+
+      cp *.so      $out/lib
+      cp *.sql     $out/share/postgresql/extension
+      cp *.control $out/share/postgresql/extension
+    '';
+}
